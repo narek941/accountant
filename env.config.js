30c4949c74cc85dce4f87f649335015872615c5e
hrant.abrahamyan.imshop <hrant.abrahamyan.imshop@gmail.com>
2023-08-08 16:41:17 +0400

    add prod url

--- env.config.js
+++ env.config.js
@@ -2,7 +2,7 @@ const env = process.env.ENV || 'development';
 
 const configs = {
   development: {
-    API: 'http://192.168.31.153:4000/api-docs',
+    API: 'https://www.acc-accountant.am/api-docs',
     EMAIL_USER: 'accforms2345678@gmail.com',
     EMAIL_PASS: 'goubmgeeyukyzjyv',
     EMAIL_ACC: 'qolyan@list.ru',
