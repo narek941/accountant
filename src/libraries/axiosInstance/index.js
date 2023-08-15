c361d1cb8f5addb62292a4a9d027b4ca04ac4de7
hrant.abrahamyan.imshop <hrant.abrahamyan.imshop@gmail.com>
2023-08-15 13:42:02 +0400

    sss

--- src/libraries/axiosInstance/index.js
+++ src/libraries/axiosInstance/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const defaultOptions = {
-  baseURL: 'http://localhost:3097',
+  baseURL: 'acc-accountant.am',
 };
 
 const axiosInstance = axios.create(defaultOptions);
